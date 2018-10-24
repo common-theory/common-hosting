a7691cb6a309cd2b53fbe32c97fc74016c3a3fe5
Chance Hudson <jchancehud@gmail.com>
2018-10-24 15:06:50 -0500

    Add comments

--- contracts/CommonHosting.sol
+++ contracts/CommonHosting.sol
@@ -1,5 +1,24 @@
 pragma solidity ^0.4.23;
 
+/**
+ * This contract is intended to provide a list of ethereum addresses mapped to
+ * domain names, mapped to ipfs addresses.
+ *
+ * Example constract representation in JSON
+ *
+ * {
+ *   "domainToPath": {
+ *     "0xddeC6C333538fCD3de7cfB56D6beed7Fd8dEE604": {
+ *       "commontheory.io": "/ipfs/QmVFrWCRoJc6yB1v6h1KQad2uw5UzcsKE9UeMNWdS5gG3V"
+ *     },
+ *     "0x0": {
+ *       "otherdomain.com": "/ipfs/QmRHT55sVPZuMQLJtTFWRXpZd9LXqmVuDtKZZVmgS2zaAJ"
+ *     }
+ *   }
+ * }
+ *
+ **/
+
 contract CommonHosting {
 
   struct HostConfig {
@@ -8,6 +27,10 @@ contract CommonHosting {
 
   mapping (address => HostConfig) configs;
 
+  /**
+   * This event _must_ be emitted when an entry changes. This allows common
+   * hosting nodes to load relevant mappings into memory and listen for updates.
+   **/
   event PathUpdate (
     address owner,
     string domain,
