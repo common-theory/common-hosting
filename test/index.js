a9d031e0f42f192602bee7e120e4473069f9eb63
Chance Hudson <jchancehud@gmail.com>
2018-11-05 12:06:58 -0600

    Add tests

---
+++ test/index.js
@@ -0,0 +1,22 @@
+const CommonHosting = artifacts.require('CommonHosting');
+
+contract('CommonHosting', accounts => {
+
+  it('should host a domain', async () => {
+    const domain = 'commontheory.io';
+    const contract = await CommonHosting.deployed();
+    await contract.storeDomain(domain, {
+      from: accounts[0],
+      // Buy 10 seconds of hosting time
+      value: 10 * await contract.hostRate.call(),
+      gasLimit: contract.storeDomain.estimateGas(domain)
+    });
+
+    assert.ok(await contract.isDomainHosted(domain));
+    // Wait 20 seconds for the purchased hosting time to run out
+    await new Promise((rs, rj) => setTimeout(rs, 20 * 1000));
+    // Then make sure the hosting runs out
+    assert.ok(await contract.isDomainHosted(domain));
+  });
+
+});
